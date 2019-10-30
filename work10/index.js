2ec9fece734aead33be937ca01c3c6e86d69a9e9
luzhenhua1165763777 <1165763777@qq.com>
2019-10-30 18:22:33 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -23,35 +23,3 @@ function monkey(){
    document.getElementById('monkeyking').innerText = monkey[0]
 
     }
-
-    function stat(){
-        let str = document.getElementById("str").value
-        let obj = {}
-    }
-        var newObj = {};
-        for (var i = 0; i < str.length; i++) {
-            if (!newObj[str.charAt(i)])
-                newObj[str.charAt(i)] = 1;
-            else
-                newObj[str.charAt(i)] += 1;
-        }
-        console.log(newObj )
-        var maxObj ={
-            maxkey :"",
-            maxvalue:0
-        }
-        for (var k in newObj) {
-            if (newObj[k] > maxObj.maxvalue) {
-                maxObj.maxvalue = newObj[k];
-                maxObj.maxkey = k;
-            }
-        }
-        return maxObj;
-    }
-    var result =getmaxObj("zhangpeiyue.com");
-    // 出现最多的字符:e
-    console.log("出现最多的字符:"+result.maxkey);
-    // 出现次数:2
-    console.log("出现次数:"+result.maxvalue);
-}
- 
